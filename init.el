7b7a71a2fffa14187e68e5bf8eed23ed0ac829d6
RePierre <petru.rebeja@gmail.com>
2016-11-30 23:27:09 +0200

    Initial folder structure; basic customization and basic packages.

---
+++ init.el
@@ -0,0 +1,13 @@
+(require 'package)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(org-babel-load-file (expand-file-name "~/.emacs.d/emacs-init.org"))
