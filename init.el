df8515d88f282b3d81e5b531b9e193d34d904c68
RePierre <petru.rebeja@gmail.com>
2017-08-28 09:56:23 +0300

    Agenda settings.

--- init.el
+++ init.el
@@ -22,9 +22,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-agenda-files (quote ("~/Dropbox/gtd/gtd.org")))
+ '(org-agenda-repeating-timestamp-show-all nil)
+ '(org-agenda-skip-deadline-if-done t)
+ '(org-agenda-skip-scheduled-if-done t)
  '(package-selected-packages
    (quote
-    (realgud use-package magit helm flycheck elpy auto-complete ace-window))))
+    (beginend graphviz-dot-mode realgud use-package magit helm flycheck elpy auto-complete ace-window))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
