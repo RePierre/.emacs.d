1b46ba966a377b940b066f52bb24476c0b62fbb5
RePierre <petru.rebeja@gmail.com>
2018-06-19 14:43:22 +0300

    Moved code for customizing agenda to a separate section in init file

--- init.el
+++ init.el
@@ -15,23 +15,3 @@
   (package-install 'use-package))
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/emacs-init.org"))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-agenda-files
-   (quote
-    ("~/Dropbox/gtd/inbox.org" "~/Dropbox/gtd/gtd.org" "~/Dropbox/gtd/tickler.org")))
- '(org-agenda-repeating-timestamp-show-all nil)
- '(org-agenda-skip-deadline-if-done t)
- '(org-agenda-skip-scheduled-if-done t)
- '(package-selected-packages
-   (quote
-    (beginend graphviz-dot-mode realgud use-package magit helm flycheck elpy auto-complete ace-window))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
