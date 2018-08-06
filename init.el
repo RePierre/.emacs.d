da5d66f5e1cd2fbb7a7b600dde47cbf9c1590146
RePierre <petru.rebeja@gmail.com>
2018-08-06 11:42:25 +0300

    Moved generated code into config.el and custom.el

--- init.el
+++ init.el
@@ -2,4 +2,8 @@
 
 (package-initialize)
 
+(let ((file-name (expand-file-name "config.el" user-emacs-directory)))
+  (if (file-exists-p file-name)
+      (load-file file-name)))
+
 (org-babel-load-file (expand-file-name "~/.emacs.d/emacs-init.org"))
