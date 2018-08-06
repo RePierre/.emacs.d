a8d7f9df04ebbfe32c789ce657c14d4036f64aa5
RePierre <petru.rebeja@gmail.com>
2018-08-06 11:33:47 +0300

    Moved initialization to a new section in the configuration file.

--- init.el
+++ init.el
@@ -1,13 +1,5 @@
 (require 'package)
-(add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/"))
-(add-to-list 'package-archives
-	     '("org" . "http://orgmode.org/elpa/") t)
 
 (package-initialize)
-(package-refresh-contents)
-
-(unless (package-installed-p 'use-package)
-  (package-install 'use-package))
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/emacs-init.org"))
