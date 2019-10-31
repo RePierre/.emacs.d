b369b578f60e791ca43a830314b01c3d295e8fd2
RePierre <petru.rebeja@gmail.com>
2019-10-31 10:56:14 +0200

    Split configuration into multiple files

--- init.el
+++ init.el
@@ -6,4 +6,6 @@
   (if (file-exists-p file-name)
       (load-file file-name)))
 
+(org-babel-load-file (expand-file-name "~/.emacs.d/common-config.org"))
+(org-babel-load-file (expand-file-name "~/.emacs.d/python-config.org"))
 (org-babel-load-file (expand-file-name "~/.emacs.d/emacs-init.org"))
