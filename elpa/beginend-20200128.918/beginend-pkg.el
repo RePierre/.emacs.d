16e5f1c99afe1e545cdcc557aa584886c2720d2b
RePierre <petru.rebeja@gmail.com>
2020-03-03 10:47:14 +0200

    Deleted old beginend versions

--- elpa/beginend-20200128.918/beginend-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "beginend" "20200128.918" "Redefine M-< and M-> for some modes" '((emacs "25.3")) :commit "52fb75259e83e4711daa5724d48775a8987395bc" :url "https://github.com/DamienCassou/beginend")
