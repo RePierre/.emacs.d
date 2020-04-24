7755dec7a7cbb3e6b398ad1a7be85dd21098a634
RePierre <petru.rebeja@gmail.com>
2020-04-24 10:07:15 +0300

    Created quail package for Romanian standard keyboard layout
    When running Emacs from WSL using X server you cannont change keyboard
    layout as usual with Win+Spc. To avoid this issue
    `toggle-input-method` can be used but there were no input methods for
    Romanian standard keyboard layout.
    
    To apply this put the following in init file:
    ```emacs-lisp
    (require 'quail-romanian-standard)
    (setq default-input-method 'romanian-standard)
    ```

---
+++ lisp/quail-romanian-standard.el
@@ -0,0 +1,39 @@
+;;; quail-romanian-standard.el --- Quail package for inputting Romanian characters -*-coding: utf-8;-*-
+
+;; Keywords: input method, romanian standard
+
+;; Author (of quail-romanian-standard.el):  Petru Rebeja <petru.rebeja@gmail.com>
+
+;;; Commentary:
+
+;; This package provides the rules for Romanian Standard
+;; keyboard layout as described here:
+;; https://en.wikipedia.org/wiki/Romanian_keyboard_layout
+
+;;; Code:
+
+(require 'quail)
+
+(quail-define-package
+ "romanian-standard" "Romanian" "RO" t
+ "Romanian Standard input method.
+" nil t nil nil nil nil nil nil nil nil t)
+
+(quail-define-rules
+ ("|" ?Â)
+ ("\\" ?â)
+ ("{" ?Ă)
+ ("[" ?ă)
+ (";" ?ş)
+ (":" ?Ş)
+ ("'" ?ţ)
+ ("\"" ?Ţ)
+ ("}" ?Î)
+ ("]" ?î)
+ ("<" ?\;)
+ (">" ?:)
+ )
+
+
+;;; quail-romanian-standard.el ends here
+(provide 'quail-romanian-standard)
