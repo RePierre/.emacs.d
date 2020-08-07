4425b5906ecc066156458dffe061fff95bd82e6e
RePierre <petru.rebeja@gmail.com>
2020-08-07 14:52:58 +0300

    Deleted old packages

--- elpa/which-key-20200208.340/which-key-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "which-key" "20200208.340" "Display available keybindings in popup" '((emacs "24.4")) :commit "6e4b4c5419b7b981da6300302278de167f76126b" :authors '(("Justin Burkett" . "justin@burkett.cc")) :maintainer '("Justin Burkett" . "justin@burkett.cc") :url "https://github.com/justbur/emacs-which-key")
