99244050b044f8a50c27eb631192bb8fd65d09de
RePierre <petru.rebeja@gmail.com>
2020-11-19 10:08:05 +0200

    Deleted old versions of packages

--- elpa/csharp-mode-20200904.922/csharp-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "csharp-mode" "20200904.922" "C# mode derived mode" 'nil :commit "5a9c8eb2e851f0ba1f5d94548afab9381e6da7bc" :keywords '("c#" "languages" "oop" "mode") :authors '(("Dylan R. E. Moonfire (original)")) :maintainer '("Jostein Kjønigsen" . "jostein@gmail.com") :url "https://github.com/josteink/csharp-mode")
--- elpa/git-commit-20200828.1753/git-commit-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "git-commit" "20200828.1753" "Edit Git commit messages" '((emacs "25.1") (dash "20200524") (transient "20200601") (with-editor "20200522")) :commit "6c1c92e5a2abbdf1c852f7da50e69f0b898e7a72" :keywords '("git" "tools" "vc") :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")
