abac180ca189580cfd2ea5088ecd0501bcadf5e1
RePierre <petru.rebeja@gmail.com>
2021-10-19 16:39:11 +0300

    Merge common-config.org into emacs-init.org

--- init.el
+++ init.el
@@ -6,11 +6,10 @@
   (if (file-exists-p file-name)
       (load-file file-name)))
 
-(org-babel-load-file (expand-file-name "~/.emacs.d/common-config.org"))
+(org-babel-load-file (expand-file-name "~/.emacs.d/emacs-init.org"))
 (org-babel-load-file (expand-file-name "~/.emacs.d/python-config.org"))
 (org-babel-load-file (expand-file-name "~/.emacs.d/js-config.org"))
 (org-babel-load-file (expand-file-name "~/.emacs.d/org-config.org"))
-(org-babel-load-file (expand-file-name "~/.emacs.d/emacs-init.org"))
 
 (let ((file-name (expand-file-name "after-init.el" user-emacs-directory)))
   (if (file-exists-p file-name)
