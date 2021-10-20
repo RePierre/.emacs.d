964a77fe3579c653d13b840525543a5f38ec682e
RePierre <petru.rebeja@gmail.com>
2021-10-20 14:41:37 +0300

    Deleted all packages before reinstall

--- elpa/helm-xref-20211013.1538/helm-xref-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from helm-xref.el  -*- no-byte-compile: t -*-
-(define-package "helm-xref" "20211013.1538" "Helm interface for xref results" '((emacs "25.1") (helm "1.9.4")) :commit "9764eabd50c40b009073c7ef64e3a71d0d066d0b" :authors '(("Fritz Stelzer" . "brotzeitmacher@gmail.com")) :maintainer '("Fritz Stelzer" . "brotzeitmacher@gmail.com") :url "https://github.com/brotzeit/helm-xref")
