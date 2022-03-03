7486cb9680e1160e49d58c00af3a3bb07ec999d8
RePierre <petru.rebeja@gmail.com>
2022-03-03 11:25:02 +0200

    Package upgrade

---
+++ elpa/forge-20220225.1417/forge-pkg.el
@@ -0,0 +1,21 @@
+(define-package "forge" "20220225.1417" "Access Git forges from Magit."
+  '((emacs "25.1")
+    (closql "1.2.0")
+    (dash "2.19.1")
+    (emacsql-sqlite "3.0.0")
+    (ghub "3.5.4")
+    (let-alist "1.0.6")
+    (magit "3.3.0")
+    (markdown-mode "2.4")
+    (transient "0.3.6")
+    (yaml "0.3.4"))
+  :commit "0f436173d1660321edac761e3e82c40e97709f63" :authors
+  '(("Jonas Bernoulli" . "jonas@bernoul.li"))
+  :maintainer
+  '("Jonas Bernoulli" . "jonas@bernoul.li")
+  :keywords
+  '("git" "tools" "vc")
+  :url "https://github.com/magit/forge")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
