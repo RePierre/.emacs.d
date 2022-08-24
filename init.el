c7223e6047fe20e254920ade83b0643696804fd9
RePierre <petru.rebeja@gmail.com>
2022-08-24 11:48:35 +0300

    Moved elfeed setup to separate file

--- init.el
+++ init.el
@@ -9,6 +9,7 @@
 (org-babel-load-file (expand-file-name "~/.emacs.d/emacs-init.org"))
 (org-babel-load-file (expand-file-name "~/.emacs.d/programming-config.org"))
 (org-babel-load-file (expand-file-name "~/.emacs.d/org-config.org"))
+(org-babel-load-file (expand-file-name "~/.emacs.d/elfeed-config.org"))
 
 (let ((file-name (expand-file-name "after-init.el" user-emacs-directory)))
   (if (file-exists-p file-name)
