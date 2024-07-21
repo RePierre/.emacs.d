47dcd9e99267b9cb8a197637066be99bc459206b
RePierre <petru.rebeja@gmail.com>
2024-07-21 18:50:37 +0300

    Added utility function to set frame transparency

--- lisp/rp-utils.el
+++ lisp/rp-utils.el
@@ -17,5 +17,17 @@
       t
     nil))
 
+(defun rp/set-transparency(arg)
+  "Set Emacs frame transparency to the value of ARG.
+
+If ARG is outside the interval [0, 100], then any value of ARG below 0
+will be considered 0, and any value above 100 will be considered 100."
+  (interactive "nEnter transparency (0 -- 100):")
+  (set-frame-parameter (selected-frame)
+		       'alpha-background
+		       (cond ((< 0 arg) 0)
+			     ((> 100 arg) 100)
+			     (t (- 100 arg)))))
+
 (provide 'rp-utils)
 ;;; rp-utils.el ends here
