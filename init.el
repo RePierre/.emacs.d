4af321cfd4ef0850b685ebfdb2768e2cea9e2cfd
RePierre <petru.rebeja@gmail.com>
2024-08-27 21:24:55 +0300

    Switched from elfeed to newsticker for RSS/Atom subscriptions

--- init.el
+++ init.el
@@ -9,7 +9,6 @@
 (org-babel-load-file (expand-file-name "~/.emacs.d/emacs-init.org"))
 (org-babel-load-file (expand-file-name "~/.emacs.d/programming-config.org"))
 (org-babel-load-file (expand-file-name "~/.emacs.d/org-config.org"))
-(org-babel-load-file (expand-file-name "~/.emacs.d/elfeed-config.org"))
 (org-babel-load-file (expand-file-name "~/.emacs.d/writing-config.org"))
 
 (let ((file-name (expand-file-name "after-init.el" user-emacs-directory)))
