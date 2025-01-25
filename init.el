456601b00c1cff1b586a9cb3ebf52fcad4bf9cf0
RePierre <petru.rebeja@gmail.com>
2025-01-25 13:07:58 +0200

    Split org-mode configuration into multiple files

--- init.el
+++ init.el
@@ -10,6 +10,9 @@
 (org-babel-load-file (expand-file-name "~/.emacs.d/programming-config.org"))
 (org-babel-load-file (expand-file-name "~/.emacs.d/writing-config.org"))
 (org-babel-load-file (expand-file-name "~/.emacs.d/org-config.org"))
+(org-babel-load-file (expand-file-name "~/.emacs.d/org-capture-config.org"))
+(org-babel-load-file (expand-file-name "~/.emacs.d/org-agenda-config.org"))
+(org-babel-load-file (expand-file-name "~/.emacs.d/org-utilities-config.org"))
 
 (let ((file-name (expand-file-name "after-init.el" user-emacs-directory)))
   (if (file-exists-p file-name)
